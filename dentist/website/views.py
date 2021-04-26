616e7791318d7b2af70959700654fea09fb63402
theorivero <thrivero1@gmail.com>
2021-04-26 20:19:52 -0300

    add contact pahe with forms

--- dentist/website/views.py
+++ dentist/website/views.py
@@ -2,3 +2,21 @@ from django.shortcuts import render
 
 def home(request):
 	return render(request, 'home.html', {})
+
+def contact(request):
+	context = {}
+	if request.method == "POST":
+		message_name = request.POST['message-name']
+		message_email = request.POST['message-email']
+		message = request.POST['message']
+
+		context = {
+			"message_name":message_name,
+		}
+
+	return render(request, 'contact.html', context)
+
+
+	
+
+		
